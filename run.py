14c7cf83685f94387b60390a886db3f852e02083
Mohamed Hagabdalla <mohamedhagabdalla@outlook.com>
2022-03-18 20:31:29 +0000

    Attempt to calculate stock numbers and add to stock worksheet

--- run.py
+++ run.py
@@ -124,8 +124,23 @@ def get_last_5_entries_sales():
         column = sales.col_values(ind)
         columns.append(column[-5:])
     
-    return columns
+    return 
+    
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
 
 def main():
     """
@@ -136,7 +151,11 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    print(stock_data)
 
 print("Welcome to Phone Shop Data Automation")
-# main()
-sales_columns = get_last_5_entries_sales()
+
+main()
+
